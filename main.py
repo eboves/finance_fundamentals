a6efd14fb774b56337af8b798e74a5b94333bfae
Elvis <e.boves@gmail.com>
2026-07-14 20:21:07 -0400

    feat: create account class

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello")
+class Account:
+    def __init__(self, name, amount, date):
+        self.name = name
+        self.amount = amount
+        self.date = date
+        self.transactions = []
+    
+    def __str__(self):
+        return f"{self.name}, Balance: ${self.amount}"
+
+amex = Account("Amex", 3245.45, "2026-07-14")
+print(amex)
