07f17c5ad667a62a63d45404069c41d90e4cf859
Elvis <e.boves@gmail.com>
2026-07-20 19:59:55 -0400

    feat: set credit_card class

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from transaction import Transaction
 amex = Account("Amex", 800, "2026-07-16")
 chase = CreditCard("Chase Sapphire", 0, "2026-07-18", 5000)
 
+
+chase.deposit(500)
 print(chase)
+
 
 # user_type = int(input("Please type 1 for deposit and 2 for withdraw: "))
 
